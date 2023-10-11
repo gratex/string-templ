9f23748c3d040227f1c1541880036150fbb121be
Marko Martin <marcus@gratex.com>
2023-10-11 13:56:34 +0200

    [NEW] if transform fails original line can be returned, see -o option

--- bin/string-templ.js
+++ bin/string-templ.js
@@ -3,27 +3,28 @@
 // example:
 // ls -1 | node ./src/cli.js '${all}' 'X-${all}-Y'
 // ls -1 | node ./src/cli.js '${all}' 'X-${all}-Y' '{"all":"(\d*)]"}'
-const VERSION = "0.0.1";
+const VERSION = "1.0.0";
 
 var program = require("commander");
 
 program
-    .version(VERSION)
-    .usage('[options] text')
-    .option('-f, --from <old-template-text>', 'from template')
-    .option('-t, --to <new-template-text>', 'to template')
-    .option('-m, --regex-map <json-format-regex-map>', 'a map for matching pattern with key')
-    .option('-e, --throws <boolean>', 'verbosely throw exception', parseBoolean)
-    .description(`Example: 
-    	string-templ -f '\${1} and \${2}' -t '\${2} scares \${1}' 'dog and bird and cat' //bird and cat scares dog
-    	string-templ -f '\${1} and \${2}' -m '{"1":".*"}' -t '\${2} scares \${1}' 'dog and bird and cat' //cat scares dog and bird`);
+  .version(VERSION)
+  .usage('[options] text')
+  .option('-f, --from <old-template-text>', 'from template')
+  .option('-t, --to <new-template-text>', 'to template')
+  .option('-m, --regex-map <json-format-regex-map>', 'a map for matching pattern with key')
+  .option('-e, --throws <boolean>', 'verbosely throw exception', parseBoolean)
+  .option('-o, --original <boolean>', 'return original line if no match (needs -e true)', parseBoolean)
+  .description(`Example: 
+        string-templ -f '\${1} and \${2}' -t '\${2} scares \${1}' 'dog and bird and cat' //bird and cat scares dog
+        string-templ -f '\${1} and \${2}' -m '{"1":".*"}' -t '\${2} scares \${1}' 'dog and bird and cat' //cat scares dog and bird`);
 
 
 program.parse(process.argv);
 
 if (!program.from || !program.to) {
-    program.help();
-    process.exit(1);
+  program.help();
+  process.exit(1);
 }
 
 var templateFrom = program.from;
@@ -35,21 +36,36 @@ var transf = require("../src/index").reformat(params, templateFrom, templateTo);
 // Process with args
 var couter = program.args.length;
 program.args.forEach(function(element, index) {
+  try {
     console.log(transf(element));
-    if (--couter === 0) { process.exit(0); }
+  } catch (ex) {
+    if (program.original) {
+      console.log(element);
+    } else {
+      throw ex;
+    }
+  }
+  if (--couter === 0) { process.exit(0); }
 });
 
 // Process with pipeline
 var byline = require("byline");
 byline(process.stdin /*,{keepEmptyLines:true}*/ ) //
-    .on("data", function(line) {
-        // console.log(line+"");
-        console.log(transf(line + ""));
-    }).on("end", function() {
-        // process.exit(0);
-    });
+  .on("data", function(line) {
+    try {
+      console.log(transf(line + ""));
+    } catch (ex) {
+      if (program.original) {
+        console.log(line + "");
+      } else {
+        throw ex;
+      }
+    }
+  }).on("end", function() {
+    // process.exit(0);
+  });
 
 // @Util
 function parseBoolean(value) {
-    return value.toLowerCase() === "true";
+  return value.toLowerCase() === "true";
 }
